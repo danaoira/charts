c1610f4fc3e5ad3c80f1edaa68d440909d1e925e
Dana <dana.toribio@noodle.ai>
2019-01-05 04:01:55 -0800

    Updates

---
+++ precache-manifest.b567f102d8573b518afa5802456dc58a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26724c1beee00480eb0c",
+    "url": "/charts/static/js/runtime~main.26724c1b.js"
+  },
+  {
+    "revision": "5d0fccaeaafc2fc9f4f7",
+    "url": "/charts/static/js/main.5d0fccae.chunk.js"
+  },
+  {
+    "revision": "585fc1ecb14bfa028e34",
+    "url": "/charts/static/js/1.585fc1ec.chunk.js"
+  },
+  {
+    "revision": "5d0fccaeaafc2fc9f4f7",
+    "url": "/charts/static/css/main.d6bdd122.chunk.css"
+  },
+  {
+    "revision": "dcaf1bb586e696a708f32b24ae574265",
+    "url": "/charts/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63b6d9384421adcca06449a0e4204f3b.js"
+  "/charts/precache-manifest.b567f102d8573b518afa5802456dc58a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/charts/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
