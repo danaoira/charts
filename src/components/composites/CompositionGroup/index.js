352a5b02b9581feaa352fbf119f75cbdfce4f3c6
Dana <dana.toribio@noodle.ai>
2019-01-25 00:15:37 -0800

    Add chart names, update title

--- src/components/composites/CompositionGroup/index.js
+++ src/components/composites/CompositionGroup/index.js
@@ -5,15 +5,25 @@ import { Box } from '../../atoms'
 const CompositionGroup = ({ rectWidth, rectHeight, chartGroup }) => (
   <g transform={`translate(0, ${rectHeight * 6})`}>
     {range(0,7).map((d, i) => (
-      <Box
-        x={rectWidth * i + 5}
-        y={5}
-        width={rectWidth - 10}
-        height={rectHeight - 10}
-        key={`composition-${i}`}
-      >
-        <text fill={'#fff'}>{chartGroup[i].name}</text>
-      </Box>
+      <g>
+        <Box
+          x={rectWidth * i + 5}
+          y={5}
+          width={rectWidth - 10}
+          height={rectHeight - 10}
+          key={`comparison-${i}`}
+        />
+        <text
+          fontSize={12}
+          x={i * rectWidth + (rectWidth / 2)}
+          y={rectHeight / 2 - 2.5}
+          fill={'#fff'}
+          textAnchor={'middle'}
+          alignmentBaseline={'middle'}
+        >
+          {chartGroup[i].name.slice(0,20)}
+        </text>
+      </g>
     ))}
   </g>
 )
