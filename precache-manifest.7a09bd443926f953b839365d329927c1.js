6b1f8cf9825092ee42ba7dddc149bb9693080628
Dana <dana.toribio@noodle.ai>
2019-01-25 00:26:12 -0800

    Updates

---
+++ precache-manifest.7a09bd443926f953b839365d329927c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26724c1beee00480eb0c",
+    "url": "/charts/static/js/runtime~main.26724c1b.js"
+  },
+  {
+    "revision": "817fa6ea4c5688463f69",
+    "url": "/charts/static/js/main.817fa6ea.chunk.js"
+  },
+  {
+    "revision": "585fc1ecb14bfa028e34",
+    "url": "/charts/static/js/1.585fc1ec.chunk.js"
+  },
+  {
+    "revision": "817fa6ea4c5688463f69",
+    "url": "/charts/static/css/main.d6bdd122.chunk.css"
+  },
+  {
+    "revision": "e8d567ec8e02c20f786a967dbd1d778b",
+    "url": "/charts/index.html"
+  }
+];
